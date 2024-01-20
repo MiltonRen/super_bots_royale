872f5b0c2544e6dd49d899df294df80232a4e246
Milton Ren <milton.x.ren@gmail.com>
2024-01-20 01:25:56 -0800

    chore: ui

--- app/jobs/generate_image_job.rb
+++ app/jobs/generate_image_job.rb
@@ -4,7 +4,7 @@ class GenerateImageJob < ApplicationJob
   def perform(bot_id)
     @bot = Bot.find(bot_id)
     task_id = EdenClient.new.create_with_concept(
-      "Battle figure named #{@bot.name}; element #{@bot.element}; description: #{@bot.description}; Solid background with color of #{@bot.element}; 3D; MINIMALISTIC AND CUTE;"
+      "Battle figure named #{@bot.name}; element #{@bot.element}; description: #{@bot.description}; Solid background with color of #{@bot.element}; 3D; MINIMALISTIC; CUTE;"
     )
     @bot.update!(image_task_id: task_id)
   end
